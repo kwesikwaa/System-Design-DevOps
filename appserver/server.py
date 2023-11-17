401c1bb40008329d83529b66758957b3ce00c674
kwesikwaa <kwesikwaahayford@gmail.com>
2023-11-17 17:52:51 +0000

    init

---
+++ appserver/server.py
@@ -0,0 +1,7 @@
+#System Design & Devops Approaches
+### simple app built with Go and Python
+### I implement techniques such as:
+### - containerizing with docker
+### - load balancing with nginx
+### - caching with redis
+### - idempotency
