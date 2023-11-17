e9e06418dfec445de6f05d94a420feb4126a4888
kwesikwaa <kwesikwaahayford@gmail.com>
2023-11-17 17:59:03 +0000

    init

--- appserver/server.py
+++ appserver/server.py
@@ -1,7 +1,17 @@
-#System Design & Devops Approaches
-### simple app built with Go and Python
-### I implement techniques such as:
-### - containerizing with docker
-### - load balancing with nginx
-### - caching with redis
-### - idempotency
+from fastapi import FastAPI,Request,logger
+import uvicorn
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_methods=["*"],allow_headers=["*"])
+
+
+@app.get('/enter')
+def firstfxn():
+    return {"here":"akwaaba"}
+
+
+
+if __name__ =="__main__":
+    uvicorn.run(app,reload = True, port= 8000, host="0.0.0.0")    
---
+++ readme.md
@@ -0,0 +1,11 @@
+#System Design & Devops Approaches
+### simple app built with Go and Python
+
+### I implement techniques such as:
+### - containerizing with docker
+### - load balancing with nginx
+### - caching with redis
+### - idempotency
+### - provisioning with terraform
+### - configuring with ansible
+### - github actions
