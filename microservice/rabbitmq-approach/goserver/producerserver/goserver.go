7800bba8f1ed58d75382979ef7b2ec8ab4cc6fc3
kwesikwaa <kwesikwaahayford@gmail.com>
2023-11-23 03:45:06 +0000

    refactored the gin server

--- microservice/rabbitmq-approach/goserver/producerserver/goserver.go
+++ microservice/rabbitmq-approach/goserver/producerserver/goserver.go
@@ -1,14 +1,63 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/streadway/amqp"
 )
 
-func main() {
-	fmt.Println("init...")
+type Order struct {
+	Userid      string  `json:"userid"`
+	Orderid     string  `json:"orderid"`
+	Productname string  `json:"productname"`
+	Quantity    int     `json:"quantity"`
+	Price       float64 `json:"price"`
+	Total       float64 `json:"total"`
+}
 
+func main() {
+
+	rest := gin.Default()
+
+	rest.POST("/order", orderHandler)
+
+	err := rest.Run(":8000")
+	if err != nil {
+		// log error message
+	}
+}
+
+func orderHandler(c *gin.Context) {
+	// message := c.Param("message")
+	//assumin the message came through as bellow
+	message := Order{
+		Userid:      "jlkj3332k",
+		Orderid:     "dsfklsjf113",
+		Productname: "toy car",
+		Quantity:    4,
+		Price:       25.00,
+		Total:       100.00,
+	}
+
+	// order := Order
+	// if err := c.ShouldBindJSON(&order); err != nil{
+	// 	c.JSON(http.StatusBadRequest, gin.H({"error":err.Error()}))
+	// 	return
+	// }
+
+	if err := queueHandler(message); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to submit Order"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Order successfully placed"})
+}
+
+func queueHandler(order Order) error {
+	//create connection
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		fmt.Println(err)
@@ -19,6 +68,7 @@ func main() {
 
 	defer conn.Close()
 
+	//create channel
 	ch, err := conn.Channel()
 	if err != nil {
 		fmt.Println(err)
@@ -26,7 +76,8 @@ func main() {
 	}
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare("FirstQueue", false, false, false, false, nil)
+	//declare a queue
+	q, err := ch.QueueDeclare("Order_Queue", false, false, false, false, nil)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -34,10 +85,16 @@ func main() {
 
 	fmt.Println(q)
 
+	// convert
+	corder, err := json.Marshal(order)
+	if err != nil {
+		// log
+	}
+	//send something to the queue
 	err = ch.Publish(
-		"", "FirstQueue", false, false, amqp.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte("Chale Im on your queue"),
+		"", q.Name, false, false, amqp.Publishing{
+			ContentType: "application/json",
+			Body:        corder,
 		},
 	)
 
@@ -45,7 +102,7 @@ func main() {
 		fmt.Println(err)
 		panic(err)
 	}
-
 	fmt.Println("Successfully published")
 
+	return nil
 }
